b0fa544eedc3848a2715538e21ae109af4aa1c8f
WangYaxi2002 <1784826320@qq.com>
2023-07-31 11:06:05 +0800

    0731

---
+++ code/曼哈顿距离.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import AgglomerativeClustering
+
+
+# 导入原始数据
+def load_plyl_data():
+    # 看见dataFrame的所有行，不然会省略
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    rawData = pd.read_csv(filepath_or_buffer='../data/BAI246_CAL_GR_AC_SP.csv', header=0, encoding='utf-8',
+                          dtype=np.float32, on_bad_lines='skip')
+    return rawData
+
+
+# 获取训练数据
+def get_train_scale(data_x):
+    num_pipeline = Pipeline([('std_scaler', MinMaxScaler())])
+    train_scale = num_pipeline.fit_transform(data_x)
+    return train_scale
+
+
+# 使用层次聚类算法
+def agglomerativeClustering(train_scale):
+    ac = AgglomerativeClustering(n_clusters=5, metric='manhattan', linkage='complete')
+    clustering = ac.fit(train_scale)
+    
+    # 保存聚类结果到csv文件
+    # with open('res.csv', 'a') as fp:
+    #     for i in clustering.labels_:
+    #         fp.write(str(i))
+    #         fp.write('\n')
+    # res = list(clustering.labels_)
+    return clustering
+
+
+# 保存图片
+def saveImage(labels: list):
+    # 对6种组合（12种结果）保存图片
+    for i in range(4):
+        for j in range(4):
+            if i == j:
+                continue
+            df = pd.DataFrame(data_x[:, [i, j]])
+            df['labels'] = labels
+            df1 = df[df['labels'] == 0]
+            df2 = df[df['labels'] == 1]
+            df3 = df[df['labels'] == 2]
+            df4 = df[df['labels'] == 3]
+            df5 = df[df['labels'] == 4]
+            plt.figure(figsize=(10, 10))
+            plt.plot(df1[0], df1[1], 'bo', label='簇1')
+            plt.plot(df2[0], df2[1], 'g*', label='簇2')
+            plt.plot(df3[0], df3[1], 'r*', label='簇3')
+            plt.plot(df4[0], df4[1], 'c*', label='簇4')
+            plt.plot(df5[0], df5[1], 'm*', label='簇5')
+            plt.xlabel(data.columns.values[i + 1])
+            plt.ylabel(data.columns.values[j + 1])
+            plt.savefig('../img/曼哈顿距离res' + str(i * 4 + j) + '.png')
+            plt.show()
+
+
+if __name__ == '__main__':
+    data = load_plyl_data()
+    data_x = data.values[:, 1:5]
+    train_scale = get_train_scale(data_x)
+    clustering = agglomerativeClustering(train_scale)
+    saveImage(list(clustering.labels_))
---
+++ code/树形图.py
@@ -0,0 +1,74 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import AgglomerativeClustering
+
+
+# 导入原始数据
+def load_plyl_data():
+    # 看见dataFrame的所有行，不然会省略
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
+    rawData = pd.read_csv(filepath_or_buffer='../data/BAI246_CAL_GR_AC_SP.csv', header=0, encoding='utf-8',
+                          dtype=np.float32, on_bad_lines='skip')
+    return rawData
+
+
+# 获取训练数据
+def get_train_scale(data_x):
+    num_pipeline = Pipeline([('std_scaler', MinMaxScaler())])
+    train_scale = num_pipeline.fit_transform(data_x)
+    return train_scale
+
+
+# 使用层次聚类算法
+def agglomerativeClustering(train_scale):
+    ac = AgglomerativeClustering(distance_threshold=0, n_clusters=None)  # , distance_threshold = 0
+    clustering = ac.fit(train_scale)
+    # 保存聚类结果到csv文件
+    # with open('../data/树形图res.csv', 'a') as fp:
+    #     for i in clustering.labels_:
+    #         fp.write(str(i))
+    #         fp.write('\n')
+    res = list(clustering.labels_)
+    return clustering, ac
+
+
+def plot_dendrogram(model, **kwargs):
+    #  创建链接矩阵，然后绘制树状图
+    #  创建每个节点的样本计数
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # 叶子节点
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+    linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                      counts]).astype(float)
+    print(linkage_matrix)
+    #  绘制相应的树状图
+    dendrogram(linkage_matrix, **kwargs)
+
+
+# 保存图片
+
+
+if __name__ == '__main__':
+    data = load_plyl_data()
+    data_x = data.values[:, 1:5]
+    train_scale = get_train_scale(data_x)
+    clustering, model = agglomerativeClustering(train_scale)
+    # saveImage(train_scale, list(clustering.labels_))
+
+    # plt.title('Hierarchical Clustering Dendrogram')
+    plot_dendrogram(model, truncate_mode='level', p=5)
+    # plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+    plt.savefig('../img/树形图.png')
+    plt.show()
